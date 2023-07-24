e1ca7f6b099156dcf0a8198d60b791335dd144fa
William Rey-Pommier <williamreypommier@gmail.fr>
2023-07-24 09:46:26 +0200

    header/footer ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { MainMenuComponent } from './mainMenu/mainMenu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ValidatedPaymentComponent } from './validatedPayment/validatedPayment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ValidatedPaymentComponent,
   ],
   imports: [
     BrowserModule,
