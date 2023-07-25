d97201f9694bab9c9a276ae2bdf3b2eaf0f00dc5
FullStackDev97 <emiliosensuavictor@gmail.com>
2023-07-25 21:19:34 +0200

    transactions filter add v1

--- src/app/account-transaction/account-transaction.component.ts
+++ src/app/account-transaction/account-transaction.component.ts
@@ -14,18 +14,70 @@ import { ApiService } from '../services/api_service/api.service';
 export class AccountTransactionComponent implements OnInit {
   account_id!: number;
   transactions :any[] = [];
+  showed_transactions : any[] = [];
+  typeOp: string = '';
+  from !: Date;
+  to !: Date;
 
   constructor(private activatedRoute: ActivatedRoute, private apiService : ApiService) { }
 
   ngOnInit(): void {
     this.account_id = parseInt( this.activatedRoute.snapshot.paramMap.get('account_id') || '0');
-    this.apiService.getTransactions(this.account_id).subscribe(data => this.transactions =[data]  );
+    this.apiService.getTransactions(this.account_id).subscribe(data => this.transactions =[...data]  );
+    this.apiService.getTransactions(this.account_id).subscribe(data => this.showed_transactions =[...data]  );
   }
 
  
 
-  showTransactions(){
-    console.log(this.transactions[0])
+  resetTransactions(){
+    this.showed_transactions = this.transactions;
+  }
+
+  filterTransacs(){
+
+    this.showed_transactions = [];
+    this.transactions.forEach((element: { date: any; montant:number }) => {
+
+      if(this.typeOp == ''){
+        if (element.date >= this.from && element.date<= this.to ) {
+          console.log(element);
+          this.showed_transactions.push(element);
+          
+        }
+      }else if (this.typeOp == 'credit') {
+        if (element.date >= this.from && element.date<= this.to && element.montant >0 ) {
+          console.log(element);
+          this.showed_transactions.push(element);
+          
+        }
+      }else if (this.typeOp == 'debit') {
+        if (element.date >= this.from && element.date<= this.to && element.montant <0 ) {
+          console.log(element);
+          this.showed_transactions.push(element);
+          
+        }
+      }
+
+      if (this.from == undefined || this.to == undefined ) {
+        if (this.typeOp == 'debit') {
+          if ( element.montant <0 ) {
+            console.log(element);
+            this.showed_transactions.push(element);
+            
+          }
+        }else if(this.typeOp == 'debit'){
+          if ( element.montant <0 ) {
+            console.log(element);
+            this.showed_transactions.push(element);
+            
+          }
+        }else{
+          this.showed_transactions = this.transactions;
+        }
+      }
+
+      
+    });
   }
 
 
